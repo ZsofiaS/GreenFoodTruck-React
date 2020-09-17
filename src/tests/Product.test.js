08b3ae3e174e70122b7e6e7c6fbb261b3e83c3b6
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-17 16:28:27 +0100

    Add unit tests for Product component

--- src/tests/Product.test.js
+++ src/tests/Product.test.js
@@ -7,4 +7,14 @@ describe('renders Product component', () => {
   it('renders without crashing', () => {
     shallow(<Product />);
   });
+  it('receives name prop', () => {
+    const product1 = shallow(<Product name="Coffee" />);
+    const coffee = <p>Coffee</p>;
+    expect(product1.contains(coffee)).toEqual(true);
+  });
+  it('receives price prop', () => {
+    const product2 = shallow(<Product price="2.5" />);
+    const price = <p>2.5</p>;
+    expect(product2.contains(price)).toEqual(true);
+  });
 });

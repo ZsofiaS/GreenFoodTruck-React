f2fc577404a888250d56498d88b6b77763747e69
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-17 16:14:15 +0100

    Add styling to buttons & container

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../styles/Product.scss';
 
 const Product = ({ name, price, img }) => (
-  <div role="button" tabIndex={0}>
-    <img src={img} alt={name} />
+  <div role="button" tabIndex={0} className="button">
+    <div className="image-container">
+      <img src={img} alt={name} className="image" />
+    </div>
     <p>{name}</p>
     <p>{price}</p>
   </div>

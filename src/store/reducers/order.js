d2faebc138b7a90f929a1c96c943fb1c8670a9cb
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-18 14:22:18 +0100

    Amend reducer - when product is saved, it is saved with the total in the store

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,20 +1,45 @@
 /* eslint-disable no-case-declarations */
 import { products } from '../../constants/Products';
+import ProductAdded from '../../models/ProductAdded';
 
 const initialState = {
   products,
-  bill: [],
+  order: {},
+  totalAmount: 0,
 };
 
 const orderReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PRODUCT':
-      const addedProduct = state.products.find(
-        (product) => product.id === action.id
-      );
-      return { ...state, bill: state.bill.concat(addedProduct) };
+      const prod = action.product;
+      const productName = prod.name;
+      const productPrice = prod.price;
+      const productId = prod.id;
+
+      let productToBeAdded;
+
+      if (state.order[productId]) {
+        productToBeAdded = new ProductAdded(
+          state.order[productId].quantity + 1,
+          productPrice,
+          productName,
+          state.order[productId].sum + productPrice
+        );
+      } else {
+        productToBeAdded = new ProductAdded(
+          1,
+          productPrice,
+          productName,
+          productPrice
+        );
+      }
+      return {
+        ...state,
+        order: { ...state.order, [productId]: productToBeAdded },
+        totalAmount: state.totalAmount + productPrice,
+      };
     case 'CANCEL_ORDER':
-      return { ...state, bill: [] };
+      return { ...state, order: {}, totalAmount: 0 };
     default:
       return state;
   }

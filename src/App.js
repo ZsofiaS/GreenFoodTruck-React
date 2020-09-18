c22d6311ffdef144441007b8612fb00a81e34826
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-18 09:34:33 +0100

    Now products are displayed from redux state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
 import Product from './components/Product.js';
-import { products } from './constants/Products.js';
 
-const App = () => (
-  <div className="App">
-    <main className="App-main">
-      {products.map((product, id) => (
-        <Product
-          key={id}
-          name={product.name}
-          price={product.price}
-          img={product.img}
-          printProduct={() => console.log(product.name)}
-        />
-      ))}
-    </main>
-  </div>
-);
+const App = () => {
+  const availableProducts = useSelector((state) => state.bills.products);
+
+  return (
+    <div className="App">
+      <main className="App-main">
+        {availableProducts.map((product, id) => (
+          <Product
+            key={id}
+            name={product.name}
+            price={product.price}
+            img={product.img}
+            printProduct={() => console.log(product.name)}
+          />
+        ))}
+      </main>
+    </div>
+  );
+};
 
 export default App;

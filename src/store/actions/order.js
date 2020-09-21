7edbbd0c53fda586a03934cbc5edf6c052cb13a3
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-21 09:44:32 +0100

    Add Redux Thunk & order is now saved into database from store

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -3,8 +3,28 @@ export const addProduct = (item) => ({
   product: item,
 });
 export const cancelOrder = () => ({ type: 'CANCEL_ORDER' });
-export const saveOrder = (items, sum) => ({
-  type: 'SAVE_ORDER',
-  products: items,
-  total: sum,
-});
+export const saveOrder = (items, sum, date = new Date()) => async (
+  dispatch
+) => {
+  const response = await fetch(
+    'https://green-food-truck.firebaseio.com/orders.json',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        items,
+        sum,
+        date,
+      }),
+    }
+  );
+  const resData = await response.json();
+  console.log(resData);
+  dispatch({
+    type: 'SAVE_ORDER',
+    products: items,
+    total: sum,
+  });
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import orderReducer from './reducers/order';
 
 const rootReducer = combineReducers({
   order: orderReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default store;

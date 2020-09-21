6f1d7e5186aaf4900edfb23bc67eb793958aa0e6
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-21 15:28:22 +0100

    Fix linting error in App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 import {
-  LineChart,
   CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
-  Legend,
-  Line,
   ResponsiveContainer,
   BarChart,
   Bar,

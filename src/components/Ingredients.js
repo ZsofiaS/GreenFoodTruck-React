93dfe518ffe975cb42ebeae98fa914988904ddb5
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-09-29 09:29:05 +0100

    Add purchase button to ingredients

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,17 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import '../styles/Ingredients.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { fetchIngredients } from '../store/actions/order';
+import Button from './Button.js';
 
 const Ingredients = () => {
   const ingredients = useSelector((state) => state.order.ingredients);
   const dispatch = useDispatch();
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const makePurchase = (item) => {
+    setSelected(item);
+    setIsOpen(!isOpen);
+  };
+
   useEffect(() => {
     dispatch(fetchIngredients());
   }, [dispatch]);
+
+  console.log(isOpen);
 
   return (
     <section className="Ingredients">
@@ -26,7 +37,7 @@ const Ingredients = () => {
         };
 
         return (
-          <div className="ingredient-container">
+          <div className="ingredient-container" key={name}>
             <p>
               <FontAwesomeIcon
                 className="coffee"
@@ -37,9 +48,18 @@ const Ingredients = () => {
               {name}: <span className="ingredient">{amount}</span>
               {isRunningLow() ? <span className="low">Low in stock</span> : ''}
             </p>
+            <Button text="Purchase" actionOrder={() => makePurchase(item)} />
           </div>
         );
       })}
+      {isOpen ? (
+        <div className="modal">
+          <h1>Open!</h1>
+          <p>{selected}</p>
+        </div>
+      ) : (
+        ''
+      )}
     </section>
   );
 };
